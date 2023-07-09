f5e0788370f6efea5becbc92917756429e5ad591
Skane-Gahi <skanguy17@orange.fr>
2023-07-09 10:15:16 +0200

    chore: moved scripts from EWW to Hyprland config

---
+++ themes/hypros/scripts/volume.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Get the volume level as integer
+get_level () {
+    # Get the volume from wpctl and extract the value
+    local wpctl_output=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -oE '[0-9]+([.][0-9]+)?')
+    # Multiply the value by 100
+    local float_level=$(echo "$wpctl_output * 100" | bc)
+    # Cast it to an integer
+    local int_level=$(printf "%.0f" "$float_level")
+
+    # return the integer
+    echo $int_level
+}
+
+# Check argument to know what to do
+if [ $1 == "up" ]; then
+    # Increase volume by 10%
+    wpctl set-volume -l 2.0 @DEFAULT_AUDIO_SINK@ 10%+
+fi
+if [ $1 == "down" ]; then
+    # Decrease volume by 10%
+    wpctl set-volume -l 2.0 @DEFAULT_AUDIO_SINK@ 10%-
+fi
+
+# Retrieve the new volume level
+volume_level=$(get_level)
+
+# Check if the widget is already opened
+eww_window=$(eww windows | grep "volume")
+is_opened=${eww_window:0:1}
+
+# Open the widget if not opened
+if [ "$is_opened" == "v" ]; then
+    eww open volume
+fi
+# Update the eww variable
+eww update volume-level=$volume_level
+
+# Wait 2 secs
+sleep 2
+# Check if the volume has changed
+new_volume_level=$(get_level)
+# If not, close the widget
+if [ "$new_volume_level" == "$volume_level" ]; then
+    eww close volume
+fi
